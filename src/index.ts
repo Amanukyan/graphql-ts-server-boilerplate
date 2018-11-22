1df646324d041cbf4ea7713d2d3bbbb85b3d6075
alex <amanukya@student.42.fr>
2018-11-22 17:01:36 +1100

    Setting up TypeORM

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+import { GraphQLServer } from 'graphql-yoga'
+import { resolvers } from "./resolvers"
+import { createConnection } from "typeorm";
+
+const server = new GraphQLServer({ typeDefs:'src/schema.graphql', resolvers })
+
+createConnection().then(() => {
+    server.start(() => console.log('Server is running on localhost:4000'))
+})
